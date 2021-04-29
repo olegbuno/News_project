cad864d9e8392a7b1593ca56a85bc2bbea181823
BunoOleg <65267379+BunoOleg@users.noreply.github.com>
2021-04-29 14:39:51 +0300

    third commit

--- news_project/settings.py
+++ news_project/settings.py
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'news_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'apps/core/templates/core'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
