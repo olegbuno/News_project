eedd9a46da2bd7bcbbe333f55ad5ef8c2419bae0
BunoOleg <65267379+BunoOleg@users.noreply.github.com>
2021-04-29 14:23:23 +0300

    second commit

--- news_project/settings.py
+++ news_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'o5t6s&hza0*)r*k#w!tbq9fa4=l*cb%upg3z@%qkpbyvuh+8!='
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'frontpage'
